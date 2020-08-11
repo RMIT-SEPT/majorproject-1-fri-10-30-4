c69e50f0aa149cf8464ee192abacbceb52196e6f
MichaelLyberopoulos <s3787260@student.rmit.edu.au>
2020-08-12 01:14:23 +1000

    loginPage

---
+++ FrontEnd/loginPage/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <form id="form" class="form">
+        <h1>Login </h1>
+
+        <div class="comps">
+          <label for="fname">Email</label>
+          <input type="text" id="email" placeholder="Email" />
+        </div>
+        <div class="comps">
+          <label for="password">Password</label>
+          <input type="password" id="password" placeholder="Password" />
+        </div>
+        <button type="login">LOGIN</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
