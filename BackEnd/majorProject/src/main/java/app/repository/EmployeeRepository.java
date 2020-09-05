bb40c9fb4a343ff2a80496b4ba85ed340618de14
s3783287 <s3783287@student.rmit.edu.au>
2020-09-06 04:09:00 +1000

    Update EmployeeRepository.java

--- BackEnd/majorProject/src/main/java/app/repository/EmployeeRepository.java
+++ BackEnd/majorProject/src/main/java/app/repository/EmployeeRepository.java
@@ -11,19 +11,5 @@ import java.util.Collection;
 
 @Repository
 public interface EmployeeRepository extends UserRepository<EmployeeImpl> {
-
-    /**
-     * @param userID
-     * @return Employee with the userID
-     */
-    @Query("SELECT employees FROM EmployeeImpl employees WHERE (userID = :userID)")
-    EmployeeImpl getEmployeeByID(@Param("userID") int userID);
-
-    /**
-     *
-     * @param businessID
-     * @return A collection of Employees with the same businessIDs
-     */
-    @Query("SELECT employees FROM EmployeeImpl employees WHERE (businessID = :businessID)")
-    Collection<EmployeeImpl> getEmployeeByBusinessID(@Param("businessID") int businessID);
+    
 }
