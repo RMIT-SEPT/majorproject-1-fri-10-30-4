23f9e5e7f49e4ddc8f38aba89fd603c465f5dca3
Nikita Phung <nikitaphung2000@gmail.com>
2020-09-07 19:23:01 +1000

    Styled Login Form

--- FrontEnd/major-project/src/components/user/Login.js
+++ FrontEnd/major-project/src/components/user/Login.js
@@ -1,22 +1,26 @@
 import React from "react"
 import {NavLink} from "react-router-dom"
-
+import "../../css/Login.css"
 function Login() {
     return (
-        <div className="App">
-        <form id="form" class="form">
-          <h1>Login </h1>
-          <div class="comps">
-            <label for="fname">Email</label>
-            <input type="text" id="email" placeholder="Email" />
-          </div>
-          <div class="comps">
-            <label for="password">Password</label>
-            <input type="password" id="password" placeholder="Password" />
-          </div>
-          <button type="login">LOGIN</button><br></br>
-         <NavLink to="/registration">Click here to register</NavLink> 
-        </form>
+        <div>
+            <div className="title">
+                <h1>Login </h1>
+            </div>
+            <form id="form" className="form">
+            <hr></hr>
+            <div className="form-group">
+                <label for="email">Email</label>
+                <input type="text" className="form-control" name="email" id="email" placeholder="Email"/>
+            </div>
+            <div className="form-group">
+                <label for="password">Password</label>
+                <input type="password" className="form-control" name="password" id="password" placeholder="Password"  />
+            </div>
+            <button type="login" className="btn btn-primary">LOGIN</button><br></br>
+            <br></br>
+            <NavLink to="/registration">Click here to register</NavLink> 
+            </form>
       </div>
     )
 }
