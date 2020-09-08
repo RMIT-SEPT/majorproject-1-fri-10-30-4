4175ff0051269aa838d03bb886830b8faea1db8c
Nikita Phung <nikitaphung2000@gmail.com>
2020-09-08 15:32:17 +1000

    Styled Employy Page

--- FrontEnd/major-project/src/components/admin/Employee.js
+++ FrontEnd/major-project/src/components/admin/Employee.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react"
+import "../../css/EmployeePage.css"
+import "../../css/Loading.css"
 const axios = require('axios').default;
 
 // async function makeGetRequest() {
@@ -12,19 +14,20 @@ class Employee extends Component {
         super(props)
         this.state = {
             employee: {},
-            success: false
+            loading: true
         }
     }
 
     componentDidMount() {
-        //makeGetRequest();
+        this.setState({loading: true})
         axios.get(`http://localhost:8080/employee/${this.props.match.params.userId}`)
-        .then(response => {
+        .then(response =>{
+            const employeeData = response.data
             this.setState(
                 prevState => { 
                     return {
-                        employee: response.data,
-                        success: !prevState.success
+                        employee: employeeData,
+                        loading: !prevState.loading
                     }
                 }
               )
@@ -32,12 +35,61 @@ class Employee extends Component {
     }
 
     render() {
-       const display = this.state.success ? "success" : "fail"
+        const {employee } = this.state;
+        const loading = () => {
+            return (
+                <div className="loader"></div>
+            )
+        }
+
+        const getDetails = () => {
+            return (
+                <div>
+                    <h1 className="title ">Employee Details</h1><hr></hr>
+                    <div className="main">
+                        <div className="row">
+                            <div className="column">
+                                <p><b>First Name</b>:</p>
+                            </div>
+                            <div className="column">
+                                <p>{employee.userFirstName}</p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="column">
+                                <p><b>Last Name:</b></p>
+                            </div>
+                            <div className="column">
+                                <p>{employee.userLastName}</p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="column">
+                                <p><b>Email:</b></p>
+                            </div>
+                            <div className="column">
+                                <p>{employee.userEmail}</p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="column">
+                                <p><b>Phone Number:</b></p>
+                            </div>
+                            <div className="column">
+                                <p>{employee.phoneNumber}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+        const renderPage = this.state.loading ? loading() : getDetails()
+
+
         return (
-        <div>
-            <h1>{display}</h1>
-            <h2>{this.state.employee.userId}</h2>
-        </div>
+            <div>
+                {renderPage}
+            </div>
         )
     }
 }
