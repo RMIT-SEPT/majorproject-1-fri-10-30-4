8297ca5166afc43c6c02b4f2b51cc26c3224d698
Joel Tan <s3758729@student.rmit.edu.au>
2020-09-11 10:54:23 +1000

    Simple Admin page added

--- FrontEnd/major-project/src/components/admin/Profile.js
+++ FrontEnd/major-project/src/components/admin/Profile.js
@@ -1,14 +1,31 @@
 import React, { Component } from "react"
+import "../../css/AdminProfile.css"
 
 class Profile extends Component {
     constructor() {
         super()
-        this.state = {}
+        this.state = {
+            
+        }
     }
 
     render() {
+        
         return(
-            <h1>Admin Profile Page</h1>
+            <div>
+                <div className="top-half">
+                    <div>
+                        <img src="../../img/default_profilepic.png"/>
+
+                    </div>
+                    <div>
+                        <h4>Admin ID</h4>
+                        <h4>Admin Name</h4>
+                        <h4>Admin Email</h4>
+                    </div>
+                </div>
+            </div>
+
         )
     }
 }
