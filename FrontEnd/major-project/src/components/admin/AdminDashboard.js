a0ef31fb30a9ad639cab6687e07ef1204bb2c1a5
Jasper Huang s3423585 <s3423585@student.rmit.edu.au>
2020-09-17 18:52:07 +1000

    Added simple admin dashboard with employee buttons connected

---
+++ FrontEnd/major-project/src/components/admin/AdminDashboard.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+import '../../css/AdminDashboard.css'
+
+function AdminDashboard() {
+    return (
+        <div>
+            <h1 className="dashboard-title">Admin Dashboard</h1>
+            <hr/>
+            <div className="container">
+                    <div className="comps">
+                        <label>Employees</label>
+                        <NavLink to="/employee/add">
+                            <button>Add</button>
+                        </NavLink>
+                        <NavLink to="/employee-list">
+                            <button>View / Edit / Remove</button>
+                        </NavLink>
+                    </div>
+                    {/*
+                    <div className="comps">
+                        <label>Bookings</label>
+                        <button>Add</button>
+                        <button>View</button>
+                    </div>
+                    <div className="comps">
+                        <label>Workers' Availabilities</label>
+                        <button>View all</button>
+                    </div>
+                    */}
+            </div>
+        </div>
+    )
+}
+
+export default AdminDashboard
