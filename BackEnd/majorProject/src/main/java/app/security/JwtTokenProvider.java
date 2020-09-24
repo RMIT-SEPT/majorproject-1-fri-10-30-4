8dc3b888d5d628ff2f61e6d467ddc395a46cec33
s3783287 <s3783287@student.rmit.edu.au>
2020-09-24 18:26:13 +1000

    Updated Security Package

---
+++ BackEnd/majorProject/src/main/java/app/security/JwtTokenProvider.java
@@ -0,0 +1,66 @@
+package app.security;
+
+import app.entity.user.Customer;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.*;
+import java.security.SignatureException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import static app.security.SecurityContants.EXPIRATION_TIME;
+import static app.security.SecurityContants.SECRET;
+
+@Component
+public class JwtTokenProvider {
+    //Generate the token
+
+    public String generateToken(Authentication authentication){
+        Customer user = (Customer)authentication.getPrincipal();
+        Date now = new Date(System.currentTimeMillis());
+
+        Date expiryDate = new Date(now.getTime()+EXPIRATION_TIME);
+
+        String userId = Long.toString(user.getCustomerID());
+
+        Map<String,Object> claims = new HashMap<>();
+        claims.put("id", (Long.toString(user.getCustomerID())));
+        claims.put("username", user.getUsername());
+        claims.put("fullName", user.getFullName());
+
+        return Jwts.builder()
+                .setSubject(userId)
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, SECRET)
+                .compact();
+    }
+
+    //Validate the token
+    public boolean validateToken(String token){
+        try{
+            Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token);
+            return true;
+        } catch (MalformedJwtException ex){
+            System.out.println("Invalid JWT Token");
+        }catch (ExpiredJwtException ex){
+            System.out.println("Expired JWT token");
+        }catch (UnsupportedJwtException ex){
+            System.out.println("Unsupported JWT token");
+        }catch (IllegalArgumentException ex){
+            System.out.println("JWT claims string is empty");
+        }
+        return false;
+    }
+
+
+    //Get user Id from token
+
+    public Long getUserIdFromJWT(String token){
+        Claims claims = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody();
+        String id = (String)claims.get("id");
+
+        return Long.parseLong(id);
+    }
+}
