421b35ebee6fa2a5d01612c108d4b0bfe54f782e
Jasper Huang s3423585 <s3423585@student.rmit.edu.au>
2020-10-07 00:44:47 +1100

    Updated pom.xml so tests work with MockitoExtension, updated BookingServiceTest and EmployeeServiceTest

--- BackEnd/majorProject/src/test/java/app/service/BookingServiceTest.java
+++ BackEnd/majorProject/src/test/java/app/service/BookingServiceTest.java
@@ -2,65 +2,51 @@ package app.service;
 
 import app.entity.Booking;
 import app.repository.BookingRepository;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
 
 /**
  * @author Jasper Huang s3423585
  */
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class BookingServiceTest {
 
-    private AutoCloseable closeable;
     private List<Booking> bookings;
 
     @BeforeEach
     void init() {
-        closeable = MockitoAnnotations.openMocks(this);
-
         Booking booking1 = new Booking();
-        Booking booking2 = new Booking();
         booking1.setBookingId(1);
-        booking2.setBookingId(2);
-
         bookings = new ArrayList<>();
         bookings.add(booking1);
-        bookings.add(booking2);
-    }
-
-    @AfterEach
-    void finalise() throws Exception {
-        closeable.close();
     }
 
     @Mock
-    private BookingRepository mockedBookingRepository;
+    private BookingRepository mockBookingRepository;
 
     @InjectMocks
     private BookingService bookingService;
 
     @Test
-    public void removeBooking_True_IfBookingIdExists() {
-        doReturn(bookings).when(mockedBookingRepository).findAll();
-        assertTrue(bookingService.removeBooking(2));
+    void removeBooking_True_IfBookingIdExists() {
+        when(mockBookingRepository.findAll()).thenReturn(bookings);
+        assertTrue(bookingService.removeBooking(1));
     }
 
     @Test
     public void removeBooking_False_IfBookingIdDoesNotExist() {
-        doReturn(bookings).when(mockedBookingRepository).findAll();
-        assertFalse(bookingService.removeBooking(3));
+        when(mockBookingRepository.findAll()).thenReturn(bookings);
+        assertFalse(bookingService.removeBooking(2));
     }
 }
